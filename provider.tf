d9b386d3d678ef5679fd16052120fdb45a5a2ead
mchand10 <mitcmohan2200@gmail.com>
2019-04-13 15:23:02 -0500

    send state to aws s3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraformwordpress"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
 provider "aws" { 
     region = "${var.AWS_REGION}"
 }
